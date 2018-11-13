c36288fbd1c8ab61052f718421240c87e8d7916e
Irza Pulungan <irzaip@gmail.com>
2018-11-13 10:42:29 +0700

    test 1

--- cipijs.py
+++ cipijs.py
@@ -11,6 +11,10 @@ import json
 from rivescript import RiveScript
 import requests
 
+if "FLASK_SETTINGS_MODULE" not in os.environ:
+    os.environ['FLASK_SETTINGS_MODULE'] = 'cipijs.settings'
+
+
 # Set up the RiveScript bot. This loads the replies from `/eg/brain` of the
 # git repository.
 bot = RiveScript()
@@ -19,9 +23,30 @@ bot.load_directory(
 )
 bot.sort_replies()
 
-app = Flask(__name__)
+class ReverseProxied(object):
 
-JS = ""
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        # print((start_response, environ,), file=sys.stdout)
+        return self.app(environ, start_response)
+
+app = Flask(__name__)
+app.config.from_envvar('FLASK_SETTINGS_MODULE')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+print(app.config)
 
 @app.route('/favicon.ico')
 def favicon():
@@ -31,7 +56,7 @@ def favicon():
 
 @app.route('/<string:page_name>/')
 def static_page(page_name):
-    return render_template('%s.html' % page_name, js=JS)
+    return render_template('%s.html' % page_name)
 
 @app.route("/reply", methods=["POST"])
 def reply():
